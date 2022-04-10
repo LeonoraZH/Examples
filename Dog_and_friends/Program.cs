ac403435de76d57d4f774ea696edf468453be4bb
Leonora <norka_200@mail.ru>
2022-04-10 20:37:10 +0300

    add Dog and Friends

---
+++ Dog_and_friends/Program.cs
@@ -0,0 +1,32 @@
+﻿int distance = 10000;
+int friendSpeed1 = 1;
+int friendSpeed2 = 2;
+int dogSpeed = 5;
+int count = 0;
+int time = 0;
+
+string x = "к 1 другу";
+string y = "к 2 другу";
+
+string dogDirect = x;
+
+while (distance > 10)
+{
+    if (dogDirect == x)
+    {
+        time = (distance / (friendSpeed1 + dogSpeed));
+        dogDirect = y;
+    }
+
+    else
+    {
+        time = (distance / (friendSpeed2 + dogSpeed));
+        dogDirect = x;
+
+    }
+
+distance -= ((friendSpeed1 + friendSpeed2) * time);
+count++;
+
+}
+Console.Write("Собака встретиться с друзьями " + count + " раз.");
